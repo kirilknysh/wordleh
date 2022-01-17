56264222cc774a4451aa0dd5ccf05b9749db67c5
Kiril Knysh <k.knysh@gmail.com>
2022-01-17 21:22:50 +0100

    Add eslint; Extract helpers;

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  'env': {
+    'browser': false,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'globals': {
+    'console': 'readonly'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
